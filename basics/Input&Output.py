9f51f7d02134fe5542a1143a055a5eb7dd131abc
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-08-27 18:52:41 +0530

    Create Input&Output.py

---
+++ basics/Input&Output.py
@@ -0,0 +1,14 @@
+#So here how i learn Input Output
+Input("Enter your name")
+#This is typical input code 
+#but if I want to take a data and store it in something then
+Name = Input("Enter your name")
+#here whatever the person write will store in Name
+#Intersting fact is that the Input value will always be string 
+#So you have to convert it yourself
+#like this example here
+age = Input("Age")
+new_age = int(age) + 1
+print(float(new_age)
+#this will give the output of age but in decimal
+#So thats it for Input & Output
