5cbe9596c3b3f311778eca4bfd6c011292f68495
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-08-28 17:18:50 +0530

    Create Strings.py

---
+++ basics/Strings.py
@@ -0,0 +1,13 @@
+Name = "Maverick"
+grade = "A"
+#This is giivng a variable string
+print(name.upper())
+#This will print the variable name in uppercase
+print(name.lower())
+#This will print the variable name in lowercase
+print(name)
+#Strings are immutable
+print(name.find("eri"))
+#It will give the index of eri in the variable name
+print(name replace("Mav","Mov"))
+#This will replace the String and output will be "Moverick"
