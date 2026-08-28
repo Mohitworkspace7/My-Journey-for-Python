ad9b7f0d79324148db61ebcf1aec5d3d170be9f1
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-08-28 08:28:01 +0530

    Create Variables.py

---
+++ basics/Variables.py
@@ -0,0 +1,12 @@
+a = "mohit"
+# The strings are always in qoutes
+b = 67
+#This is an integer variable
+c = True
+#This is a boolean variable
+d = 67.8
+#This is float variable
+#There are 4 types of variables as i have learned i.e, int, float, bool, string
+#if i want to know the type then i have to write this
+print(type(name))
+#so it will show me the type of variable the variable name is holding
