22c962687ca8369bc5bb1e1183ba624b46f7ce50
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-08-30 17:38:27 +0530

    Create  If,If else,Elif.py

---
+++ If,If else,Elif and Loops/ If,If else,Elif.py
@@ -0,0 +1,20 @@
+#If means Agar in hindi it is conditional it gives contion to the code
+#here is an example
+age = 24
+if age >= 18:
+  print("You are an Adult")
+#See here i have used : it is because print comes under if condition
+#next is elif like 
+age = 24
+if age >= 18:
+  print("You are an Adult")
+elif age < 18:
+    print("You are not an Adult")
+#Another condition is called else we can use else when nothing else to make condition of like i  this example
+age = 24
+if age >= 18:
+  print("You are an Adult")
+else:
+    print("You are not an Adult")
+    #see here we have not given any condition
+#but the rest of which does not satisfy the condition hets the else loop
