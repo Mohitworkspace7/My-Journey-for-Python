8773f063850313d98c291357c0ee276a9b750e55
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-08-30 10:11:08 +0530

    Create Simple Math.py

---
+++ basics/Simple Math.py
@@ -0,0 +1,15 @@
+#Simple maths in Python
+#First take numbers in variables
+a = 10
+b = 70
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(b%a)
+#This is module that means remainder after division
+#ques- After taking a number give its square and cube
+a = input("Your number")
+a = int(a) #Here we are converting the number from strings to integer
+print(a*a)
+print(a*a*a)
