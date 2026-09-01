4239eaf58ce3289c7e9c223692f2e48eea6a2405
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-01 19:18:06 +0530

    Create Largest Number.py

---
+++ Additional practice questions/Level 3/Largest Number.py
@@ -0,0 +1,11 @@
+a = int(input("First number = "))
+b = int(input("Second number = "))
+c = int(input("Third number = "))
+if a>b and a>c:
+    print(a, "is largest number")
+elif b>a and b>c:
+    print(b, "is largest number")
+elif c>a and c>b:
+    print(c, "is largest number")
+    
+#use of and is new to me so I will put in python mistake book
