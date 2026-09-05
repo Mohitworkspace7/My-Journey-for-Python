e5cdc6f0169dd588273a51a3e0ab1b570a2e3aad
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-05 18:10:24 +0530

    Create Calculator.py

---
+++ Additional practice questions/Level 3/Calculator.py
@@ -0,0 +1,13 @@
+a = int(input("First number"))
+b = int(input("Second number"))
+Optr = input("Operator only +-/*")
+if Optr == "+":
+    print(a+b)
+elif Optr == "-":
+    print(a-b)
+elif Potr == "/":
+    print(a/b)
+elif Optr == "*":
+    print(a*b)
+else:
+    print("Invalid")
