7f2c37a542b025ce0d576d6588c080280884448b
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-07 19:25:09 +0530

    Create 1-100 sum.py

---
+++ Additional practice questions/Level 4/1-100 sum.py
@@ -0,0 +1,7 @@
+i = 0 # i ko define kiya h
+sum = 0 # sum ko define kiya h
+while i <= 99: # ye loop start kiya 100 tk
+    i += 1 # i ko 1 usey increase
+    sum = i + sum #sum mene define kiya phir usey aur i ko add krke suk mai store kr diya
+print(sum)
+#it feel awesome man
