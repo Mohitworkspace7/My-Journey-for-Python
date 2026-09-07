c40191289b4f80e9229414d123630bbae264b6a5
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-07 19:17:04 +0530

    Create while loop.py

---
+++ If,If else,Elif and Loops/while loop.py
@@ -0,0 +1,8 @@
+i = 1
+#tb tk print hoga jb tk i 10 se jyda nhi ho jata
+while i <= 10:
+    print(i)
+    i = i + 1
+    #i += 1 bhi likh skte h
+    i ki value increase aur decrease krna bhot jaruri hota h
+    
