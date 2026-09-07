d492a048392e16ec00c537dfc921a2ed07f7a95c
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-07 19:30:45 +0530

    Create Multiplication table .py

---
+++ Additional practice questions/Level 4/Multiplication table .py
@@ -0,0 +1,8 @@
+n = int(input("Number")) # number input liya
+table = 0 #define table
+i = 1 # yha jarurat pdi jisey print krunga
+while table < 10: # loop
+    table += 1 #increment of 1 
+    i = n * table # yha mene store ki multiplication ki value
+    print(i) # within loop print kiya taaki table print ho na ki last number
+ 
