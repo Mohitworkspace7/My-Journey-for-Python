eea6d70ea0e562772d664adfc19beea7275035c9
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-10 12:07:32 +0530

    Create Triple Factorial.py

---
+++ Additional practice questions/Level 4/Triple Factorial.py
@@ -0,0 +1,9 @@
+#isme -3 krte h jbtk 3,2,1 na aajaye
+n = int(input("Triple factorial "))
+fact = 1
+i = 0
+while n >=3 or n >=2 or n>=1:
+    fact= fact * n
+    n -= 3
+print(fact)
+#i think i am starting to understand the concept
