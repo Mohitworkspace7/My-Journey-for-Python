8cd33bedde55a3c306c965ac1b52ef94be54b4ec
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-13 17:25:25 +0530

    Create realpyramid.py

---
+++ Additional practice questions/Level 5/realpyramid.py
@@ -0,0 +1,9 @@
+x=5
+for i in range(1,6):
+    for n in range(1,x+1):
+        print(end=" ")
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+    x-=1
+    #end=" " mai space dena jaruri h bhot
