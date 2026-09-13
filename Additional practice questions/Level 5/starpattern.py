3e4a71f3ae128d8d5aa9673e3518f86a554f84bf
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-13 07:34:04 +0530

    Create starpattern.py

---
+++ Additional practice questions/Level 5/starpattern.py
@@ -0,0 +1,6 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end ="")
+    print()
+    #this isdifferent fro the first one
+    #i am learning it becoz mujhe aur bhi sikhne h
