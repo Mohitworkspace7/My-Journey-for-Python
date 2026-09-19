bc298e1d4c3db0756f72821e087c059d85320337
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-19 08:18:35 +0530

    Create bool().py

---
+++ Built in Functions/bool().py
@@ -0,0 +1,8 @@
+print(bool(1))          # True
+print(bool(0))          # False
+print(bool(""))         # False (empty string)
+print(bool("hello"))    # True (non-empty string)
+print(bool([]))         # False (empty list)
+print(bool([1, 2]))     # True (non-empty list)
+print(bool(None))       # False
+#it converts to only false or true
