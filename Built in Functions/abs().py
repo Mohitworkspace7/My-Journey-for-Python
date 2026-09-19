0f54af4dd6464e46b7271f4e24ab0e58f68d2941
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-19 08:08:27 +0530

    Create abs().py

---
+++ Built in Functions/abs().py
@@ -0,0 +1,6 @@
+print(abs(-5))      # 5
+print(abs(5))       # 5
+print(abs(-3.14))   # 3.14
+print(abs(3 + 4j))  # 5.0 (magnitude of complex number)
+#it always gives positive number
+#distance from 0 is absolute
