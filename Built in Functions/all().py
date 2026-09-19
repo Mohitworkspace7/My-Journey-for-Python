255fa0f09960dae49e9a669bb327684ad24a8889
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-19 08:14:04 +0530

    Create all().py

---
+++ Built in Functions/all().py
@@ -0,0 +1,7 @@
+# Practical example
+nums = [2, 4, 6, 8]
+print(all(n % 2 == 0 for n in nums))  # True, all even
+
+#another example
+nums = [2, 4, 6, 8]
+print(all(n % 3 == 0 for n in nums))  # True, all even
