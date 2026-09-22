5eba92f2d572c0e19e2226000c12d5bee208356e
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-22 11:31:23 +0530

    Create slicing.py

---
+++ basics/slicing.py
@@ -0,0 +1,8 @@
+seq[start:stop:step]
+#where seq can be list tuples or strings
+#where start is index
+#stop is last index
+# step is step how much step do
+#negative means reverse
+#example reverse seq[::-1]
+#index start from 0 always not 1
