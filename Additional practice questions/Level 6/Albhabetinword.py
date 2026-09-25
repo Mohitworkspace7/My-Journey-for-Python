0144d5c553ff03c452f6ea4d409cb54d5f24e39a
Mohitworkspace7 <rockeekeshav@gmail.com>
2026-09-25 08:03:56 +0530

    Create Albhabetinword.py

---
+++ Additional practice questions/Level 6/Albhabetinword.py
@@ -0,0 +1,9 @@
+n = input("Enter a string: ")
+j = input("Enter a albhabet: ")
+count = 0
+#count mtlb counter h
+for i in n:
+    if i in j:
+        count += 1
+        
+print("Character:", count)
